8336a5477109d82f825d8c3487bbe04f20269077
Chris Chambers <cchambers@gmail.com>
2018-11-29 20:15:58 -0500

    vanqs

---
+++ mods/NOS/scripts/globals/server_settings/executioner.lua
@@ -0,0 +1,65 @@
+--[[
+	Executioner is a mod applied to weapons to make them more powerful against certain mobile team types.
+]]
+
+ServerSettings.Executioner = {
+ 
+	-- Modifier applied to final damage of Executioner weapons when hitting the correct MobileKind for the weapon
+	LevelModifier = {
+		1.25, -- Ruin
+		1.50, -- Might
+		1.75, -- Force
+		2.00, -- Power
+		2.25, -- Vanquishing
+	},
+
+	-- This will be put on the tooltip of the weapon and %s will be replaced with the MobileKind of the weapon.
+	LevelString = {
+		"%s of Ruin (+1)",
+		"%s of Might (+3)",
+		"%s of Force (+5)",
+		"%s of Power (+7)",
+		"%s of Vanquishing (+9)",
+	},
+
+	-- The default intensity of an executioner weapon when created randomly
+	DefaultIntensity = {0, 50},
+	-- List of templates that are possible to be created via random_executioner_weapon.lua
+	RandomTemplateList = {
+		"weapon_katana",
+		"weapon_longsword",
+		"weapon_broadsword",
+		"weapon_saber",
+		"weapon_greataxe",
+		"weapon_largeaxe",
+		"weapon_mace",
+		"weapon_maul",
+		"weapon_hammer",
+		"weapon_quarterstaff",
+		"weapon_warhammer",
+		"weapon_warfork",
+		"weapon_spear",
+		"weapon_voulge",
+		"weapon_halberd",
+		"weapon_shortbow",
+		"weapon_longbow",
+		"weapon_warbow",
+		"weapon_dagger",
+		"weapon_kryss",
+		"weapon_poniard",
+		"weapon_bone_dagger",
+	},
+	-- List of all possible MobileKind types for a weapon created via random_executioner_weapon.lua
+	RandomMobileKinds = {
+		"Undead",
+		-- "Dragon",
+		"Humanoid",
+		-- "Reptile",
+		-- "Ork",
+		-- "Arachnid",
+		-- "Animal",
+		-- "Demon",
+		-- "Giant",
+		--"Ent",
+	},
+}
