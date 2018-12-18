a28efd096b7b305253582697cf3e4f3ccc0d1846
Chris Chambers <cchambers@gmail.com>
2018-12-18 16:31:13 -0500

    final for merge

--- mods/NOS/scripts/sp_pillar_of_fire_effect.lua
+++ mods/NOS/scripts/sp_pillar_of_fire_effect.lua
@@ -1,14 +1,10 @@
 RegisterEventHandler(EventType.Message,"SpellHitUserEffectsp_pillar_of_fire_effect",
     function (target)
-        DebugMessage("One")
         target:PlayEffect("FireTornadoEffect")
-        
         CallFunctionDelayed(TimeSpan.FromSeconds(1.5), function() 
             EndEffect(target)
         end);
-
     end)
-
 
 function EndEffect(target)
     target:StopEffect("FireTornadoEffect")
