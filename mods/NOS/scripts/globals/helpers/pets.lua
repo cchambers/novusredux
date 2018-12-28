bd26d1d588e27484a37b64a3e70c2c018b7320dd
Chris Chambers <cchambers@gmail.com>
2018-12-28 16:17:43 -0500

    merge with master, beastmastery tweak

--- mods/NOS/scripts/globals/helpers/pets.lua
+++ mods/NOS/scripts/globals/helpers/pets.lua
@@ -2,7 +2,7 @@ require 'default:globals.helpers.pets'
 
 function MaxActivePetSlots(master)
     local slots = GetSkillLevel(master, "BeastmasterySkill")/10 or 0
-    if (slots == 0) then slots = 1 end
+    if (slots == 0) then slots = 2 end
     -- master:SystemMessage(tostring("You can control " .. slots .. " slots worth of pets."))
 	return slots
 end
