5109bbb2102d4e2eb3db3e3be662dce81397d93d
Chris Chambers <cchambers@gmail.com>
2018-12-28 16:21:45 -0500

    whoops

--- mods/NOS/scripts/globals/helpers/pets.lua
+++ mods/NOS/scripts/globals/helpers/pets.lua
@@ -1,8 +1,8 @@
 require 'default:globals.helpers.pets'
 
 function MaxActivePetSlots(master)
-    local slots = GetSkillLevel(master, "BeastmasterySkill")/10 or 0
-    if (slots == 0) then slots = 2 end
+    local slots = GetSkillLevel(master, "BeastmasterySkill")/10 or 2
+    if (slots < 2) then slots = 2 end
     -- master:SystemMessage(tostring("You can control " .. slots .. " slots worth of pets."))
 	return slots
 end
