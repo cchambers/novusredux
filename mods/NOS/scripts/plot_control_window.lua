4984b45dc17d92eee300f37394d9eb1b12c920a8
Chris Chambers <cchambers@gmail.com>
2018-12-28 20:22:55 -0500

    daily

--- mods/NOS/scripts/plot_control_window.lua
+++ mods/NOS/scripts/plot_control_window.lua
@@ -147,6 +147,7 @@ function CoOwnerTab(dynamicWindow)
     dynamicWindow:AddImage(8,32,"BasicWindow_Panel",314,244,"Sliced")
 
     dynamicWindow:AddButton(20,240,"AddCoOwner","Add Co-Owner",290,26,"Co-Owners can Lock/Unlock the door, use secure containers, and lock down/release items. Only in this house.","",false,"List")
+    dynamicWindow:AddButton(20,200,"OpenBank","Open Your Bank",290,26,"Open your bank for free, temporarily.","",false,"List")
     
     local coOwners = controller:GetObjVar("HouseCoOwners") or {}
     local scrollWindow = ScrollWindow(21,44,280,184,23)
@@ -271,6 +272,9 @@ RegisterEventHandler(EventType.DynamicWindowResponse, "PlotControlWindow", funct
             return -- don't cleanup
         elseif ( returnId == "AddCoOwner" ) then
             this:SendMessage("StartMobileEffect", "HouseAddCoOwner", controller)
+            return -- don't cleanup
+        elseif ( returnId == "OpenBank" ) then
+            this:SendMessage("OpenBank", controller)
             return -- don't cleanup
         end
     else
