780a210fe7160d4c57cdeac0fff533118fc74694
Chris Chambers <cchambers@gmail.com>
2018-12-29 22:13:27 -0500

    calm test

--- mods/NOS/scripts/globals/mobile_effects/nos/crookcalm.lua
+++ mods/NOS/scripts/globals/mobile_effects/nos/crookcalm.lua
@@ -6,14 +6,19 @@ MobileEffectLibrary.CrookCalm =
         if (target) then
             local hp = target:GetStatValue("Health")
             local maxhp = GetMaxHealth(target)
-            local percent = hp / maxhp
-            self.ParentObj:SystemMessage("HP " .. hp .. " .. " .. percent .. "%")
+            local percent = (hp / maxhp) * 100
+            local formula = (percent < 25)
+            if (formula) then
+                self.ParentObj:SystemMessage("It might work!")
+            else
+                self.ParentObj:SystemMessage("It probably won't work!")
+            end
         end
         EndMobileEffect(root)
     end,
 
     OnExitState = function(self, root)
-        UnregisterEventHandler(GetCurrentModule(), EventType.ClientTargetLocResponse, "Telecrook.Move")
+        self.ParentObj:SystemMessage("EXITING")
     end,
 }
  
