96802bdb5d5c1b179e6baad6f52890e1200aa162
Chris Chambers <cchambers@gmail.com>
2018-12-30 11:35:00 -0500

    mountable class wyverns

---
+++ mods/NOS/scripts/ai_demon.lua
@@ -0,0 +1,12 @@
+require 'default:ai_demon'
+
+if (this:HasObjVar("DoEffect")) then
+    local effect = this:GetObjVar("DoEffect")
+    -- local delay = this:GetObjVar("EffectDelay") or 2
+    this:ScheduleTimerDelay(TimeSpan.FromSeconds(2),"DoEffect", effect)
+end
+
+RegisterEventHandler(EventType.Timer,"DoEffect",function ( effect )
+    this:PlayEffect(effect, 2.5)
+    this:ScheduleTimerDelay(TimeSpan.FromSeconds(5),"DoEffect", effect)
+end)
