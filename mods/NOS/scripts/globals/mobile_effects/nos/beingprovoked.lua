ef6e7b852f975460c5cce45b89456f5ed6d1c2e3
Chris Chambers <cchambers@gmail.com>
2018-12-30 18:26:23 -0500

    Provocation

--- mods/NOS/scripts/globals/mobile_effects/nos/beingprovoked.lua
+++ mods/NOS/scripts/globals/mobile_effects/nos/beingprovoked.lua
@@ -6,14 +6,33 @@ MobileEffectLibrary.BeingProvoked =
 			EndMobileEffect(root)
 			return false
 		end
-		
 		self.Tamer = tamer
-		tamer:SystemMessage(tostring("You begin provoking the " .. self.ParentObj:GetName()), "info")
-  
+
+		self.Tamer:SystemMessage(tostring("You begin provoking the " .. self.ParentObj:GetName()), "info")
+
+		RegisterSingleEventHandler(EventType.ClientTargetLocResponse, "Crook.Provoke",
+		function (success, targetLoc, targetObj, user)
+			if (success) then
+				local target = targetObj
+				if (target:IsMobile()) then
+					self.Target = target
+					self.ParentObj:SendMessage("AttackEnemy", target, true)
+				else 
+					self.Tamer:SystemMessage("You are going to have a hard time trying to do that.", "info")
+					EndMobileEffect(root)
+				end
+			end
+		end)
+
+		self.Tamer:SystemMessage("What should it attack?", "info")
+		self.Tamer:RequestClientTargetLoc(self.ParentObj, "Crook.Provoke")
 	end,
 
 	OnExitState = function(self,root)
 		self.Tamer:SystemMessage(tostring("Provocation has ended.", "info"))
+		if (self.ParentObj:GetStatValue("Health") > self.Target:GetStatValue("Health")) then
+			self.ParentObj:SendMessage("EndCombatMessage")
+		end
 	end,
 
 	GetPulseFrequency = function(self,root)
@@ -25,5 +44,5 @@ MobileEffectLibrary.BeingProvoked =
 	end,
 
 	Tamer = nil,
-	Duration = TimeSpan.FromSeconds(10)
+	Duration = TimeSpan.FromSeconds(30)
 }
