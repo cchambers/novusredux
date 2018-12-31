806e675d98933b493fab37a4df3acdda53ca5035
Chris Chambers <cchambers@gmail.com>
2018-12-30 19:06:52 -0500

    lunar mobs only work at night

--- mods/NOS/scripts/ai_lunar.lua
+++ mods/NOS/scripts/ai_lunar.lua
@@ -1,7 +1,7 @@
 this:ScheduleTimerDelay(TimeSpan.FromSeconds(60), "StillNightTime")
 
 RegisterEventHandler(EventType.Timer, "StillNightTime", function () 
-    if (IsNightTime()) then
+    if (IsNightTime() and not(IsPet(this))) then
         this:ScheduleTimerDelay(TimeSpan.FromSeconds(60), "StillNightTime")
     elseif (IsPet(this)) then
         this:DelModule("ai_lunar")
