5fca632ced9e06245cca23d894d86d26f3a7d0dd
Chris Chambers <cchambers@gmail.com>
2019-01-01 02:18:57 -0500

    stables broken somehow

--- mods/NOS/scripts/globals/helpers/pets.lua
+++ mods/NOS/scripts/globals/helpers/pets.lua
@@ -1,5 +1,13 @@
 require 'default:globals.helpers.pets'
 
+function GetRemainingActivePetSlots(master)
+    Verbose("Pet", "GetRemainingActivePetSlots", master)
+    local pets, slots = GetActivePets(master, nil, true)
+	local test = MaxActivePetSlots(master) - slots
+	DebugMessage(tostring("TEST " .. test))
+	return MaxActivePetSlots(master) - test
+end
+
 function MaxActivePetSlots(master)
     local slots = GetSkillLevel(master, "BeastmasterySkill")/10 or 1
     if (slots < 1) then slots = 1 end
