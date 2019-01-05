82005621c72d7490a1ed37fa96313b38b03ce2ab
Chris Chambers <cchambers@gmail.com>
2019-01-05 16:51:30 -0500

    test wtih doc

--- mods/NOS/scripts/sp_reveal_effect.lua
+++ mods/NOS/scripts/sp_reveal_effect.lua
@@ -1,35 +1,35 @@
 mLoc = nil
 
-RegisterEventHandler(
-	EventType.Message,
-	"CompletionEffectsp_reveal_effect",
-	function()
-		if(mLoc == nil) then 
-			mLoc = this:GetLoc()
-		end
-		local mobiles =
-			FindObjects(
-			SearchMulti(
-				{
-					SearchRange(mLoc, 20),
-					SearchMobile()
-				}
-			),
-			GameObj(0)
-		)
-		for i, v in pairs(mobiles) do
-			if (not IsDead(v)) then
-				if (HasMobileEffect(v, "Hide")) then
-					v:SendMessage("StartMobileEffect", "Revealed")
-				end
-			end
-		end
-
-		CallFunctionDelayed(
-			TimeSpan.FromSeconds(1),
-			function()
-				this:DelModule("sp_reveal_effect")
-			end
-		)
+function DoRevealStuff()
+	this:NpcSpeech("TEST")
+	if (mLoc == nil) then
+		mLoc = this:GetLoc()
 	end
-)
+	local mobiles =
+		FindObjects(
+		SearchMulti(
+			{
+				SearchRange(mLoc, 20),
+				SearchMobile()
+			}
+		),
+		GameObj(0)
+	)
+	for i, v in pairs(mobiles) do
+		if (not IsDead(v)) then
+			if (HasMobileEffect(v, "Hide")) then
+				v:SendMessage("StartMobileEffect", "Revealed")
+			end
+		end
+	end
+
+	CallFunctionDelayed(
+		TimeSpan.FromSeconds(10),
+		function()
+			this:DelModule("sp_reveal_effect")
+		end
+	)
+end
+
+RegisterSingleEventHandler(EventType.ModuleAttached, "sp_reveal_effect", DoRevealStuff)
+-- RegisterEventHandler(EventType.Message, "CompletionEffectsp_reveal_effect", DoRevealStuff)
