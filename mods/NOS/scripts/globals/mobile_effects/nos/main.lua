3c30c6049de80075bc9268ab446fb428b41775b6
Chris Chambers <cchambers@gmail.com>
2019-01-05 15:59:35 -0500

    add revealed mobile effect

--- mods/NOS/scripts/globals/mobile_effects/nos/main.lua
+++ mods/NOS/scripts/globals/mobile_effects/nos/main.lua
@@ -1,6 +1,7 @@
 require 'globals.mobile_effects.nos.telecrookmove'
 require 'globals.mobile_effects.nos.powerhour'
 require 'globals.mobile_effects.nos.murderer'
+require 'globals.mobile_effects.nos.revealed'
 require 'globals.mobile_effects.nos.crookcalm'
 require 'globals.mobile_effects.nos.beingcalmed'
 require 'globals.mobile_effects.nos.crookprovoke'
