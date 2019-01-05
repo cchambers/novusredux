e9ead4aeedc23fad5b4e0378bd290707416e989c
Chris Chambers <cchambers@gmail.com>
2019-01-04 21:16:16 -0500

    dragons hoard

--- mods/NOS/scripts/globals/helpers/combat.lua
+++ mods/NOS/scripts/globals/helpers/combat.lua
@@ -1,13 +1,5 @@
 require 'default:globals.helpers.combat'
 
-function CheckActiveSpellResist(target)
-    -- 0 to 50% chance, 10 will = 0, 50 will = 50%
-	if ( Success(0.5*((( GetWis(target) or 0 ) - ServerSettings.Stats.IndividualStatMin) / (ServerSettings.Stats.IndividualPlayerStatCap - ServerSettings.Stats.IndividualStatMin))) ) then
-		return true
-	end
-	return false
-end
-
 function DoResist(target, resistLevel, damage)
     local resistAmount = (resistLevel * 10 - 400) / 15
     -- target:SystemMessage(tostring("Damage: " .. damage .. " Resist: " .. resistAmount))
