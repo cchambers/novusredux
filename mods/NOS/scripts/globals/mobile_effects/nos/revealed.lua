bfda291680fe035b1da1764931db4a486da6800d
Chris Chambers <cchambers@gmail.com>
2019-01-05 16:22:12 -0500

    more tweaks

--- mods/NOS/scripts/globals/mobile_effects/nos/revealed.lua
+++ mods/NOS/scripts/globals/mobile_effects/nos/revealed.lua
@@ -2,15 +2,13 @@ MobileEffectLibrary.Revealed =
 {
 	OnEnterState = function(self,root,target,args)
         self.ParentObj:SendMessage("BreakInvisEffect")
-		-- self.ParentObj:PlayAnimation("dance_gangamstyle")
+		self.ParentObj:PlayAnimation("kneel_standup")
         self.ParentObj:NpcSpeech("*gasp*")
-        AddBuffIcon(this,"RevealedSpell","Weaken","Thunder Strike 04", "You have been revealed!")
         -- local skillLevel = GetSkillLevel(self.ParentObj,"MagerySkill")
 	end,
 
 	OnExitState = function(self,root)
-	    RemoveBuffIcon(this,"RevealedSpell")
-    --self.ParentObj:PlayAnimation("kneel_standup")
+		return
 	end,
 
 	GetPulseFrequency = function(self,root)
--- mods/NOS/scripts/sp_reveal_effect.lua
+++ mods/NOS/scripts/sp_reveal_effect.lua
@@ -1,14 +1,33 @@
-RegisterEventHandler(EventType.Message, "CompletionEffectsp_reveal_effect",
-	function ()
-		if (HasMobileEffect(this, "Hide")) then
-			this:NpcSpeech("AM HIDDEN")
-		else
-			this:NpcSpeech("SAYS NOT HIDDEN")
+RegisterEventHandler(
+	EventType.Message,
+	"CompletionEffectsp_reveal_effect",
+	function()
+		local mobiles =
+			FindObjects(
+			SearchMulti(
+				{
+					SearchRange(mLoc, 20),
+					SearchMobile()
+				}
+			),
+			GameObj(0)
+		)
+		for i, v in pairs(mobiles) do
+			if (not IsDead(v)) then
+				if (HasMobileEffect(v, "Hide")) then
+					v:NpcSpeech("OH SHIT... HI.")
+				else
+					v:NpcSpeech("But I'm not hidden.")
+				end
+				v:SendMessage("StartMobileEffect", "Revealed")
+			end
 		end
-		
-		this:SendMessage("StartMobileEffect", "Revealed")
 
-		CallFunctionDelayed(TimeSpan.FromSeconds(1), function ()
-			this:DelModule("sp_reveal_effect")
-		end)
-	end)
+		CallFunctionDelayed(
+			TimeSpan.FromSeconds(1),
+			function()
+				this:DelModule("sp_reveal_effect")
+			end
+		)
+	end
+)
