6e1f6bcf4a9da31b749b4d948ed4d5774fe0839b
Chris Chambers <cchambers@gmail.com>
2019-01-06 15:15:35 -0500

    defense, power spells

--- mods/NOS/scripts/sp_defense_effect.lua
+++ mods/NOS/scripts/sp_defense_effect.lua
@@ -8,33 +8,33 @@ mBuffed = false
 function HandleLoaded()
 	local skillLevel = GetSkillLevel(this,"MagerySkill")
 
-	if( this:HasTimer("SpellAgilityBonusTimer") ) then
-		this:RemoveTimer("SpellAgilityBonusTimer")
+	if( this:HasTimer("SpellDefenseBonusTimer") ) then
+		this:RemoveTimer("SpellDefenseBonusTimer")
 	end
-	mIncreaseAmount = math.floor(skillLevel/10)
+	mIncreaseAmount = math.floor(skillLevel/10) * 2
 
-	SetMobileMod(this, "AgilityPlus", "SpellAgility", mIncreaseAmount)
-	AddBuffIcon(this,"AgilitySpellBuff","Agility","Blazing Speed","Agility is increased by "..mIncreaseAmount,false,mDurationMinutes*60)
-	this:ScheduleTimerDelay(TimeSpan.FromMinutes(mDurationMinutes), "SpellAgilityBonusTimer")
+	SetMobileMod(this, "DefensePlus", "SpellDefense", mIncreaseAmount)
+	AddBuffIcon(this,"DefenseSpellBuff","Defense","Deflect","Defense is increased by "..mIncreaseAmount,false,mDurationMinutes*60)
+	this:ScheduleTimerDelay(TimeSpan.FromMinutes(mDurationMinutes), "SpellDefenseBonusTimer")
 	if not( mBuffed ) then
-		this:SystemMessage("Your Agility has increased by "..mIncreaseAmount, "event")
+		this:SystemMessage("Your Defense has increased by "..mIncreaseAmount, "event")
 	end
 	mBuffed = true
 end
 
 function CleanUp()
-	SetMobileMod(this, "AgilityPlus", "SpellAgility", nil)
-	this:SystemMessage("Agility has worn off, decreasing by "..mIncreaseAmount, "event")
-	RemoveBuffIcon(this,"AgilitySpellBuff")
+	SetMobileMod(this, "DefensePlus", "SpellDefense", nil)
+	this:SystemMessage("Defense has worn off, decreasing by "..mIncreaseAmount, "event")
+	RemoveBuffIcon(this,"DefenseSpellBuff")
 	mIncreaseAmount = 0
 	this:DelModule(GetCurrentModule())
 end
 
-RegisterEventHandler(EventType.Timer, "SpellAgilityBonusTimer", function()
+RegisterEventHandler(EventType.Timer, "SpellDefenseBonusTimer", function()
 	CleanUp()
 	end)
 
-RegisterEventHandler(EventType.Message, "SpellHitEffectsp_agility_effect", 
+RegisterEventHandler(EventType.Message, "SpellHitEffectsp_defense_effect", 
 	function(caster)
 		HandleLoaded()
 	end)
