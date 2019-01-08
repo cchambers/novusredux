829929d993b30104e408b839835e24e10c1b24f6
Chris Chambers <cchambers@gmail.com>
2019-01-07 23:22:38 -0500

    text tweak

--- mods/NOS/scripts/hud_tracker.lua
+++ mods/NOS/scripts/hud_tracker.lua
@@ -12,7 +12,7 @@ function UpdateStatsWindow()
 
 	if (total < 2) then total = 2 end
 
-	NOSCNX:AddLabel(18, 0, tostring("[bada55]" .. total .. "[-] players connected // join global chat at: [bada55]nos.gg/discord[-]"), 600, 20, 16, "left", false, true, "SpectralSC-SemiBold")
+	NOSCNX:AddLabel(18, 0, tostring("[bada55]" .. total .. "[-] players connected // join global chat at: [bada55]nos.gg/discord[-]"), 600, 20, 18, "left", false, true, "SpectralSC-SemiBold")
 	this:OpenDynamicWindow(NOSCNX)
 	this:ScheduleTimerDelay(TimeSpan.FromSeconds(2),"Hud.UpdateStats")
 end
