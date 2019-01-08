c4f0d9b1fc4ec17e82aa6c885c7d9f0470725cf1
Chris Chambers <cchambers@gmail.com>
2019-01-08 08:49:57 -0500

    fix hud tracker

--- mods/NOS/scripts/hud_tracker.lua
+++ mods/NOS/scripts/hud_tracker.lua
@@ -1,7 +1,7 @@
 function UpdateStatsWindow()
 	local ds_width = 300
 	local ds_height = 300
-	local NOSCNX = DynamicWindow("NOSCNX" .. this.Id, "Server Stats", 600, 60, 0, -15, "Transparent", "BottomLeft")
+	local NOSCNX = DynamicWindow("NOSCNXx" .. this.Id, "Server Stats", 600, 60, 0, -14, "Transparent", "BottomLeft")
 	local online = GlobalVarRead("User.Online")
 	local total = 0
 	if (online ~= nil) then
@@ -11,8 +11,8 @@ function UpdateStatsWindow()
 	end
 
 	if (total < 2) then total = 2 end
-
-	NOSCNX:AddLabel(18, 0, tostring("[bada55]" .. total .. "[-] players connected // join global chat at: [bada55]nos.gg/discord[-]"), 600, 20, 18, "left", false, true, "SpectralSC-SemiBold")
+	total = tostring(total)
+	NOSCNX:AddLabel(18, 0, tostring("[bada55]" .. total .. "[-] players connected // join global chat at: [bada55]nos.gg/discord[-]"), 1000, 20, 18, "left", true, true, "SpectralSC-SemiBold")
 	this:OpenDynamicWindow(NOSCNX)
 	this:ScheduleTimerDelay(TimeSpan.FromSeconds(2),"Hud.UpdateStats")
 end
