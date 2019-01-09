36d15447401e509be6b4ae418ebbe696998bb0cf
Chris Chambers <cchambers@gmail.com>
2019-01-09 17:11:10 -0500

    released pet shrinkage

--- mods/NOS/scripts/pet_controller.lua
+++ mods/NOS/scripts/pet_controller.lua
@@ -46,3 +46,33 @@ function UnStableSelf()
 		UserPetCommand("follow", _MyOwner)
 	end
 end
+
+function CleanUpModule()
+	this:SetObjectOwner(nil)
+	this:SetSharedObjectProperty("Title", "Released Pet")
+	RemoveTooltipEntry(this, "PetOwner", "Released Pet")
+	
+	local livingName = this:GetObjVar("LivingName")
+	if ( livingName ) then
+		this:SetName(livingName)
+	end
+	this:SetObjVar("WasTamed", true)
+
+	this:StopMoving()
+
+	this:AddModule("ai_prey")
+	this:AddModule("released_pet")
+
+	-- delete variables no longer needed
+	this:DelObjVar("controller")
+	this:DelObjVar("NoReset")
+	this:DelObjVar("MobileTeamType")
+	this:DelObjVar("CommandName")
+	this:DelObjVar("TamingDifficulty") -- no longer tamable
+	this:DelObjVar("Karma")
+
+	Decay(this)
+
+	this:SendMessage("UpdateName")
+	this:DelModule("pet_controller")
+end
