7fb8f0813b79beee0adc397462e691c71e223237
Chris Chambers <cchambers@gmail.com>
2019-01-11 09:04:08 -0500

    more potion work, portable tool

---
+++ mods/NOS/scripts/globals/mobile_effects/potions/heal.lua
@@ -0,0 +1,30 @@
+MobileEffectLibrary.PotionHeal = 
+{
+
+	OnEnterState = function(self,root,target,args)
+		self.Percentage = args.Percentage or self.Percentage
+
+		self.Amount = GetMaxHealth(self.ParentObj) * (self.Percentage * 0.01)
+		if ( self.ParentObj:HasTimer("RecentPotion") ) then
+			self.ParentObj:SystemMessage("Cannot use again yet.", "info")
+			EndMobileEffect(root)
+			return false
+		end
+		
+		if ( GetCurHealth(self.ParentObj) >= GetMaxHealth(self.ParentObj) ) then
+			self.ParentObj:SystemMessage("You seem fine.", "info")
+			EndMobileEffect(root)
+			return false
+		end
+		self.ParentObj:ScheduleTimerDelay(TimeSpan.FromMinutes(1), "RecentPotion")
+
+		self.ParentObj:PlayEffect("HealEffect")
+
+		self.ParentObj:SendMessage("HealRequest", self.Amount, target, true)
+
+		EndMobileEffect(root)
+	end,
+
+	Percentage = 1,
+	Amount = 1
+}
---
+++ mods/NOS/scripts/globals/static_data/resource_effects/potions.lua
@@ -0,0 +1,106 @@
+ResourceEffectData.PotionGreaterHeal = {
+    MobileEffect = "PotionHeal",
+    MobileEffectArgs = {
+        Amount = 200,
+    },
+    SelfOnly = true,
+    Tooltip = {
+        "Instantly restores 200 health.",
+    }
+}
+
+ResourceEffectData.PotionHeal = {
+    MobileEffect = "PotionHeal",
+    MobileEffectArgs = {
+        Amount = 100,
+    },
+    SelfOnly = true,
+    Tooltip = {
+        "Instantly restores 100 health.",
+    }
+}
+
+ResourceEffectData.PotionLesserHeal = {
+    MobileEffect = "PotionHeal",
+    MobileEffectArgs = {
+        Amount = 50,
+    },
+    SelfOnly = true,
+    Tooltip = {
+        "Instantly restores 50 health.",
+    }
+}
+
+ResourceEffectData.PotionGreaterStamina = {
+    MobileEffect = "PotionStamina",
+    MobileEffectArgs = {
+        Amount = 60,
+    },
+    SelfOnly = true,
+    Tooltip = {
+        "Instantly restores 60 stamina.",
+    }
+}
+
+ResourceEffectData.PotionStamina = {
+    MobileEffect = "PotionStamina",
+    MobileEffectArgs = {
+        Amount = 40,
+    },
+    SelfOnly = true,
+    Tooltip = {
+        "Instantly restores 40 stamina.",
+    }
+}
+
+ResourceEffectData.PotionLesserStamina = {
+    MobileEffect = "PotionStamina",
+    MobileEffectArgs = {
+        Amount = 20,
+    },
+    SelfOnly = true,
+    Tooltip = {
+        "Instantly restores 20 stamina.",
+    }
+}
+
+ResourceEffectData.PotionGreaterMana = {
+    MobileEffect = "PotionMana",
+    MobileEffectArgs = {
+        Amount = 80,
+    },
+    SelfOnly = true,
+    Tooltip = {
+        "Instantly restores 60 mana.",
+    }
+}
+
+ResourceEffectData.PotionMana = {
+    MobileEffect = "PotionMana",
+    MobileEffectArgs = {
+        Amount = 40,
+    },
+    SelfOnly = true,
+    Tooltip = {
+        "Instantly restores 40 mana.",
+    }
+}
+
+ResourceEffectData.PotionLesserMana = {
+    MobileEffect = "PotionMana",
+    MobileEffectArgs = {
+        Amount = 25,
+    },
+    SelfOnly = true,
+    Tooltip = {
+        "Instantly restores 20 mana.",
+    }
+}
+
+ResourceEffectData.PotionCure = {
+    MobileEffect = "PotionCure",
+    SelfOnly = true,
+    Tooltip = {
+        "Instantly cure yourself of poison.",
+    }
+}
