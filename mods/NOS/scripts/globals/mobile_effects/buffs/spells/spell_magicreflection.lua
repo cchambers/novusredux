4f4a80ded82f4af2e35a905ba169c85dcf806151
Chris Chambers <cchambers@gmail.com>
2019-01-10 22:52:35 -0500

    MR buff icon

--- mods/NOS/scripts/globals/mobile_effects/buffs/spells/spell_magicreflection.lua
+++ mods/NOS/scripts/globals/mobile_effects/buffs/spells/spell_magicreflection.lua
@@ -2,6 +2,10 @@ MobileEffectLibrary.SpellMagicReflection =
 {
 	OnEnterState = function(self,root,target,args)
 		self.ParentObj:SetObjVar("MagicReflection", true)
+		
+		local debuffStr = "You will reflect incoming magical attacks."
+		AddBuffIcon(self.ParentObj, "MagicReflectionBuff", "Magic Reflection", "Spell Shield", debuffStr, true)
+
 		RegisterSingleEventHandler(EventType.Message, "StopReflecting",
 		function()	
 			EndMobileEffect(root)
@@ -10,6 +14,7 @@ MobileEffectLibrary.SpellMagicReflection =
 
 	OnExitState = function(self,root)
 		self.ParentObj:DelObjVar("MagicReflection")
+		RemoveBuffIcon(self.ParentObj, "MagicReflectionBuff")
 		self.ParentObj:SystemMessage("You reflected a spell.")
 	end,
 }
