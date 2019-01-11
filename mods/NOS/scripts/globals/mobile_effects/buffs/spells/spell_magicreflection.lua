b7665d3b197cab2eaa54acf13b67846b19fe8a93
Chris Chambers <cchambers@gmail.com>
2019-01-10 22:41:29 -0500

    MR persist

--- mods/NOS/scripts/globals/mobile_effects/buffs/spells/spell_magicreflection.lua
+++ mods/NOS/scripts/globals/mobile_effects/buffs/spells/spell_magicreflection.lua
@@ -1,6 +1,7 @@
 MobileEffectLibrary.SpellMagicReflection = 
 {
 	OnEnterState = function(self,root,target,args)
+		self.ParentObj:SetObjVar("MagicReflection", true)
 		RegisterSingleEventHandler(EventType.Message, "StopReflecting",
 		function()	
 			EndMobileEffect(root)
@@ -8,6 +9,7 @@ MobileEffectLibrary.SpellMagicReflection =
 	end,
 
 	OnExitState = function(self,root)
+		self.ParentObj:DelObjVar("MagicReflection")
 		self.ParentObj:SystemMessage("You reflected a spell.")
 	end,
 }
