651f6a5404865930794ebeb7486b772eef00eec4
Chris Chambers <cchambers@gmail.com>
2019-01-13 19:05:35 -0500

    poison tweaks

--- mods/NOS/scripts/globals/mobile_effects/buffs/spells/spellpoison.lua
+++ mods/NOS/scripts/globals/mobile_effects/buffs/spells/spellpoison.lua
@@ -5,18 +5,19 @@ MobileEffectLibrary.SpellPoison =
 		self.Target = target
 
 		local magery = GetSkillLevel(self.Target, "MagerySkill")
+		local poisoning = GetSkillLevel(self.Target, "PoisoningSkill")
 		local percent = magery / ServerSettings.Skills.PlayerSkillCap.Single
 		local poisonLevel = 1
 
-		if (magery >= 65) then 
+		if (magery >= 65 and poisoning >= 65) then 
 			poisonLevel = 2
 		end
 		
-		if (magery >= 90) then 
+		if (magery >= 90 and poisoning >= 90) then 
 			poisonLevel = 3
 		end
 		
-		if (magery >= 100) then 
+		if (magery >= 100 and poisoning >= 100) then 
 			poisonLevel = 4
 		end
 
