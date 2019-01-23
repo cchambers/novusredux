95d52f071f4760daef65efba7a3a1f4579f2f555
Bryce Fosheim <ber.ycs@gmail.com>
2019-01-22 23:03:44 -0600

    Added 15min decay, and a bit of randomness to bloodmoss spawn.

--- mods/NOS/scripts/base_ai_mob.lua
+++ mods/NOS/scripts/base_ai_mob.lua
@@ -14,7 +14,7 @@ function TurnNearbyMossIntoBloodMoss()
     -- TODO - Verlorens - Globalize this call, so it's not in base_player_death.lua AND base_ai_mob.lua
     -- TODO - Verlorens - Make it a more elegant way in which moss turns into bloodmoss.  
     -- Right now it's set so if something dies close enough to it, it turns into bloodmoss.
-    -- Right now it's set so if ANY NPC dies.
+    -- Right now it's set so if ANY NPC but Undead dies.
     local nearbyMoss = FindObjects(SearchMulti(
 		{
 			SearchRange(this:GetLoc(), 8),
@@ -22,9 +22,16 @@ function TurnNearbyMossIntoBloodMoss()
         }))
     for i,j in pairs(nearbyMoss) do
         if( this:GetObjVar("MobileKind") ~= "Undead") then
-            CreateObj("plant_bloodmoss",j:GetLoc())
-            j:Destroy()
+            if(math.random(0,10) > 2) then
+                CreateTempObj("plant_bloodmoss", j:GetLoc(), "bloodmoss_created_by_death")
+                j:Destroy()
+            end
         end
     end
 end
 
+RegisterEventHandler(EventType.CreatedObject,"bloodmoss_created_by_death",function (success,objRef)
+    if (success) then
+        Decay(objRef, 900)  -- 900 seconds is 15 minutes
+	end
+end)
