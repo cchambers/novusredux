90148a8be8ad661bb17e4328a50dd3e0dddfe25b
Chris Chambers <cchambers@gmail.com>
2019-01-24 22:32:47 -0500

    Color wars templates

---
+++ mods/NOS/scripts/colorwar_item.lua
@@ -0,0 +1,5 @@
+--[[
+- Destroy when not in TestMap or TwoTowers
+- Color itself to match wielder's skin tone
+- 
+]]--
---
+++ mods/NOS/scripts/colorwar_player.lua
@@ -0,0 +1,5 @@
+--[[
+- STORE HUE
+- Change hue to team hue. 835: Bloo, 831: Lit
+- Add/track certain stats for CW
+]]--
