a564626f67e1eea4cddd6ea793be35bfa30121d4
Chris Chambers <cchambers@gmail.com>
2019-01-26 17:46:37 -0500

    really fix silver steed

--- mods/NOS/scripts/globals/mobile_effects/nos/reclaim.lua
+++ mods/NOS/scripts/globals/mobile_effects/nos/reclaim.lua
@@ -6,10 +6,11 @@ MobileEffectLibrary.Reclaim = {
             self.Target = target
             if not (IsInBackpack(target, user)) then
                 user:SystemMessage("That is not in your pack.", "info")
+                EndMobileEffect(root)
                 return false
             end
 
-            local forge =
+            self.Forge =
                 FindObjects(
                 SearchMulti(
                     {
@@ -19,78 +20,43 @@ MobileEffectLibrary.Reclaim = {
                 )
             )
 
-            if (#forge == 0) then
+            if (#self.Forge == 0) then
                 user:SystemMessage("You need a forge to do that.", "info")
+                EndMobileEffect(root)
                 return false
             else
-                self.PermanentDamage = target:GetObjVar("PermanentDamage") or 0
-                self.RepairTarget(self, target, root)
+                self.ReclaimTarget(self, target, root)
             end
         end
         return
     end,
-    RepairTarget = function(self, target, root)
+    ReclaimTarget = function(self, target, root)
         local user = self.ParentObj
-        local maxDurability = target:GetObjVar("MaxDurability")
-        local durability = target:GetObjVar("Durability") or maxDurability
-        local damage = maxDurability - durability
+        local skillLevel = GetSkillLevel(user, "MetalsmithSkill")
 
-        if (durability >= maxDurability - self.PermanentDamage) then
-            user:SystemMessage("That is already fully repaired.", "info")
-            return false
+        local repair = {0, 100}
+        if (skillLevel < 30) then
+            repair = {5, 20}
+        elseif (skillLevel < 65) then
+            repair = {35, 65}
+        elseif (skillLevel < 85) then
+            repair = {75, 90}
+        elseif (skillLevel <= 100) then
+            repair = {90, 100}
         end
-        if (CheckSkillChance(user, "MetalsmithSkill")) then
-            
-            self.StartProgressBar(self, root)
-            user:ScheduleTimerDelay(self.Duration, "Blacksmith.Repair")
 
-            local bestAmount = maxDurability - self.PermanentDamage
-            local damageDuringRepair = math.round(bestAmount * 0.01)
-
-            local skillLevel = GetSkillLevel(user, "MetalsmithSkill")
-            local repair = {0, 100}
-            if (skillLevel < 30) then
-                repair = {5, 20}
-            elseif (skillLevel < 65) then
-                repair = {35, 65}
-            elseif (skillLevel < 85) then
-                repair = {75, 90}
-            elseif (skillLevel <= 100) then
-                repair = {90, 100}
-            end
-
-            local percent = math.random(repair[1], repair[2])
-            local repairAmount = math.round(damage * (percent * 0.01))
-            self.RepairTo = repairAmount
-            self.PermanentDamage = maxDurability - repairAmount
-
-            RegisterEventHandler(
-                EventType.Timer,
-                "Blacksmith.Repair",
-                function()
-                    self.DoRepair(self, root)
-                end
-            )
-        else
-            local destroy = math.random(1, 20) == 1
-            if (destroy) then
-                user:NpcSpeech("[ff0000]*ting*[-]")
-                user:SystemMessage("You fail to repair the item and destroy it in the process.", "info")
-                target:Destroy()
-            else
-                user:SystemMessage("You fail to repair the item.", "info")
-            end
-        end
+        local percent = math.random(repair[1], repair[2]) * 0.01
+        self.RepairTo = math.round(bestAmount * percent)
         return
     end,
     StartProgressBar = function(self, root)
         ProgressBar.Show(
             {
                 TargetUser = self.ParentObj,
-                Label = "Repairing...",
+                Label = "Reclaiming...",
                 Duration = self.Duration,
                 PresetLocation = "AboveHotbar",
-                DialogId = "Repairing",
+                DialogId = "Reclaiming",
                 CanCancel = true,
                 CancelFunc = function()
                     EndMobileEffect(root)
@@ -98,15 +64,18 @@ MobileEffectLibrary.Reclaim = {
             }
         )
     end,
-    DoRepair = function(self, root)
-        self.Target:SetObjVar("PermanentDamage", self.PermanentDamage)
-        self.Target:SetObjVar("Durability", self.RepairTo)
+    DoReclaim = function(self, root)
+        -- destroy item
+        -- create ingots
+        EndMobileEffect(root)
+        return
     end,
     OnExitState = function(self, root)
         UnregisterEventHandler("", EventType.Timer, "Blacksmith.Repair")
     end,
+
+    Forge = {},
     Target = nil,
-    RepairTo = 0,
-    PermanentDamage = 0,
-    Duration = TimeSpan.FromSeconds(5)
+    Durability = 0,
+    Duration = TimeSpan.FromSeconds(3)
 }
