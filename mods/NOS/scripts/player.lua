18e8c3c981ce98a46eb417b6234dde0736b4be66
Chris Chambers <cchambers@gmail.com>
2019-01-28 18:27:21 -0500

    missed some

--- mods/NOS/scripts/player.lua
+++ mods/NOS/scripts/player.lua
@@ -702,13 +702,13 @@ end
 
 
 function HandleRequestPickUp(pickedUpObject)
-	if (this:HasObjVar("ColorwarPlayer")) then
+	-- if (this:HasObjVar("ColorwarPlayer")) then
 		if (pickedUpObject:HasObjVar("ColorwarItem")) then
 			if (not(pickedUpObject:HasModule("colorwar_flag"))) then
 				pickedUpObject:SetHue(this:GetHue())
 			end
 		end
-	end
+	-- end
 
 	-- DebugMessage("Tried to pick up "..pickedUpObject:GetName())
 	local carriedObject = this:CarriedObject()
