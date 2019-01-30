4755a74527c4f020b9dda00ed83345d89caea510
Chris Chambers <cchambers@gmail.com>
2019-01-29 20:50:03 -0500

    items and global power hour stuff

--- mods/NOS/scripts/dono_box.lua
+++ mods/NOS/scripts/dono_box.lua
@@ -2,11 +2,14 @@ require 'container'
 
 RegisterEventHandler(EventType.ContainerItemAdded, "", 
     function(addedObj)
-		
 		-- coins
 		if(addedObj:GetObjVar("ResourceType") == "coins") then
+			DebugMessage("That's a lotta dough!");
+			PowerHourDonate(addedObj:GetObjVar("StackCount"))
+			addedObj:Destroy()
     		-- get amount, credit to total, destroy.
+		else
+			addedObj:AddModule("dono_item")
 		end
 		
-
 	end)
