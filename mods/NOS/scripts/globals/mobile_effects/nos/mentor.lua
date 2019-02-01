593f4554556d0cbf1408efb72a15b415a72f6cb2
Chris Chambers <cchambers@gmail.com>
2019-01-31 23:11:46 -0500

    visual

--- mods/NOS/scripts/globals/mobile_effects/nos/mentor.lua
+++ mods/NOS/scripts/globals/mobile_effects/nos/mentor.lua
@@ -24,7 +24,7 @@ MobileEffectLibrary.Mentor = {
             )
             mMENTOR:AddButton(
                 180,
-                50,
+                40,
                 "TradeSkillType",
                 "Trade",
                 110,
@@ -42,14 +42,14 @@ MobileEffectLibrary.Mentor = {
                 "CHOOSEMENTORTYPE",
                 function(user, buttonId)
                     if (buttonId ~= nil and buttonId ~= "") then
-                        user:SetObjVar("MentorPath")
-                        self.RequestInitialTarget(self, root, target, args)
+                        user:SetObjVar("MentorPath", buttonId)
+                        self.SelectSkill(self, root, target, buttonId)
                     end
                     return
                 end
             )
-        else 
-            self.RequestInitialTarget(self, root, target, args)
+        else
+            self.SelectSkill(self, root, target, self.Path)
         end
     end,
     RequestInitialTarget = function(self, root, target, args)
@@ -62,10 +62,48 @@ MobileEffectLibrary.Mentor = {
                     "apply beingmentored mobileeffect to target and ping them every 3 seconds for 30"
                 )
                 target:SystemMessage("Targeted for learnin")
-                EndMobileEffect(root)
             end
         )
         self.ParentObj:RequestClientTargetGameObj(self.ParentObj, "Mentor.Choose")
+    end,
+    SelectSkill = function(self, root, target, path)
+        self.Path = self.Path or path
+        local newWindow = DynamicWindow("CHOOSESKILL", "You can train...", 320, 530)
+        local mentorSkills = self.ParentObj:GetObjVar("SkillDictionary") -- all of the players skills
+        local allSkills = {}
+
+        local scrollWindow = ScrollWindow(20, 40, 250, 375, 25)
+
+        for skillName, skillData in pairs(SkillData.AllSkills) do
+            if (not (skillData.Skip)) then
+                DebugMessage(tostring(self.Path .. " " .. skillName .. " " .. skillData.SkillType))
+                if (mentorSkills[skillName] ~= nil and mentorSkills[skillName].SkillLevel >= 90) then
+                    table.insert(allSkills, skillName)
+                end
+            end
+        end
+
+        for i, skillName in pairs(allSkills) do
+            local name = allSkills[i]
+            local scrollElement = ScrollElement()
+            if ((i - 1) % 2 == 1) then
+                scrollElement:AddImage(0, 0, "Blank", 230, 25, "Sliced", "1A1C2B")
+            end
+            scrollElement:AddLabel(45, 3, skillName, 0, 0, 18)
+            local selState = ""
+            if (skillName == self.selectedSkillName) then
+                selState = "pressed"
+            end
+            scrollElement:AddButton(20, 2, "select|" .. skillName, "", 0, 18, "", "", false, "Selection", selState)
+            scrollWindow:Add(scrollElement)
+        end
+        newWindow:AddScrollWindow(scrollWindow)
+
+        newWindow:AddButton(15, 420, "train|", "Train", 100, 23, "", "", false, "", buttonState)
+
+        self.ParentObj:OpenDynamicWindow(newWindow)
+
+        EndMobileEffect(root)
     end,
     OnExitState = function(self, root)
         return
