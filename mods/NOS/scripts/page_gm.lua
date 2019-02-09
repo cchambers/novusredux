036257078ca892de4109a219af97e69413204d39
Chris Chambers <cchambers@gmail.com>
2019-02-09 14:52:18 -0500

    player pages

--- mods/NOS/scripts/page_gm.lua
+++ mods/NOS/scripts/page_gm.lua
@@ -2,17 +2,20 @@ RegisterSingleEventHandler(
 	EventType.ModuleAttached,
 	"page_gm",
 	function()
-		local newWindow = DynamicWindow("PageWindow","Page a GM",400,300,200,200,"Default","TopLeft")
-
-		local x = 30
-		local y = 215
-		newWindow:AddTextField(x, 10, 320,200, "PageDetails", "Details are important! Delete this line.")
-		
-		newWindow:AddButton(x, y, "Submit", "Submit", 150, 0, "", "", true)
-		newWindow:AddButton(x+170, y, "Cancel", "Cancel", 150, 0, "", "", true)
-		--newWindow:AddButton(x, y, "CancelImprovement", "Cancel", 200, 0, "Stop Attempting to Improve the item.", "", false)
-		this:OpenDynamicWindow(newWindow,this)
-		
+		if not(this:HasTimer("NoPageTimer")) then
+			local newWindow = DynamicWindow("PageWindow","Page a GM",400,300,200,200,"Default","TopLeft")
+			local x = 30
+			local y = 215
+			newWindow:AddTextField(x, 10, 320,200, "PageDetails", "Details are important! Delete this line.")
+			
+			newWindow:AddButton(x, y, "Submit", "Submit", 150, 0, "", "", true)
+			newWindow:AddButton(x+170, y, "Cancel", "Cancel", 150, 0, "", "", true)
+			--newWindow:AddButton(x, y, "CancelImprovement", "Cancel", 200, 0, "Stop Attempting to Improve the item.", "", false)
+			this:OpenDynamicWindow(newWindow,this)
+		else 
+			this:SystemMessage("You will need to wait a bit before doing that again.", "info")
+			this:DelModule("page_gm")
+		end
 	end
 )
 
@@ -27,6 +30,7 @@ function (user,buttonId,page)
 	if ( buttonId == "Submit" ) then
 		if ( page.PageDetails == "" or page.PageDetails == bugReportStartingText ) then
 			this:SystemMessage("[D70000]Please provide more detail in your report.[-]","info")
+			this:ScheduleTimerDelay(TimeSpan.FromMinutes(5),"NoPageTimer")
 			return
 		end
 		
