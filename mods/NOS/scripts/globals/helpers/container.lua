a43f634ab469d9d875353ddb37ada4d98fd75048
Chris Chambers <cchambers@gmail.com>
2019-02-12 19:44:31 -0500

    boom

---
+++ mods/NOS/scripts/globals/helpers/container.lua
@@ -0,0 +1,82 @@
+require 'default:globals.helpers.container'
+
+
+function TryPutObjectInContainer(obj, container, locInContainer, canOverfill, tryStack, testOnly)
+    
+    local ResourceTypes = container:GetObjVar("ResourceTypes")
+    if (ResourceTypes ~= nil) then
+        local type = obj:GetObjVar("ResourceType") or nil
+        local isGood = false
+        for k, v in pairs(ResourceTypes[1]) do
+            if (ResourceTypes[1][k] == type) then
+                return true
+            end
+        end
+        
+        if (isGood == false) then
+            return false,"This container is for specific items."
+        end
+    end
+
+
+	if(obj == container or container:IsContainedBy(obj)) then
+		return false,"You can't put an object inside itself!"
+	end
+
+	local topContainer = obj:TopmostContainer()
+	local topContainerInContainer = container:TopmostContainer() or container
+	if (topContainer ~= nil and topContainer:IsPlayer() and (not topContainerInContainer:IsContainedBy(topContainer))) then
+		if (not(topContainer:HasLineOfSightToObj(topContainerInContainer,ServerSettings.Combat.LOSEyeLevel))) then
+			return false, "Container is not in sight."
+		end
+	end
+
+	local objWeight = obj:GetSharedObjectProperty("Weight") or -1
+	if(objWeight == nil or objWeight == -1) then
+		return false,"That is too heavy for that container."
+	end
+
+	-- make sure this container or its parents are not for sale
+	local isSaleContainer = false
+    ForEachParentContainerRecursive(container,true,
+        function (parentObj)
+            if(parentObj:HasModule("hireling_merchant_sale_item")) then                
+                isSaleContainer = true
+                return false
+            end
+            return true
+        end)
+
+    if(isSaleContainer) then
+        return false,"[$1853]"
+    end
+
+	if not(canOverfill) then
+		local canAdd,weightCont,maxWeight = CanAddWeightToContainer(container,objWeight)
+
+		-- if this would put any of our parent containers over their max weight then fail
+		if not(canAdd) then
+			return false,StripColorFromString(weightCont:GetName()).." cannot support any more weight. (Max: " .. tostring(maxWeight) .. " stones)"
+		end
+	end
+
+	if( tryStack ) then
+		if( TryStack(obj,container,testOnly) ) then
+			return true
+		end
+	end
+
+	if(locInContainer == nil) then
+		locInContainer = GetRandomDropPosition(container)
+	end
+
+	if( canOverfill or container:CanHold(obj) ) then
+		if not(testOnly) then
+			obj:MoveToContainer(container, locInContainer)
+		end
+		return true
+    end	
+    
+
+	return false,"There is not enough room for that object."
+end
---
+++ mods/NOS/scripts/stackable.lua
@@ -0,0 +1,51 @@
+require 'default:stackable'
+
+
+function SetStack(amount)
+	local singleWeight = this:GetObjVar("UnitWeight") or 1
+	
+	local curWeight = this:GetSharedObjectProperty("Weight")
+	local newWeight = math.max(1, amount*singleWeight)
+	local adjustWeightBy = newWeight - curWeight
+
+	this:SetObjVar("StackCount", amount)
+	UpdateStackName(amount, this)
+	if(adjustWeightBy ~= 0) then
+		this:SetSharedObjectProperty("Weight", newWeight)
+		local containedObj = this:ContainedBy()
+        if(containedObj ~= nil) then
+            if (not(containedObj:HasModule('container_magic'))) then
+                containedObj:SendMessage("AdjustWeight", adjustWeightBy)
+            end
+		end
+	end
+end
+
+function AdjustStack(delta)
+	local stackCount = this:GetObjVar("StackCount") or 1
+	local singleWeight = this:GetObjVar("UnitWeight") or 1
+	stackCount = stackCount + delta
+	if( stackCount <= 0 ) then
+		this:Destroy()
+	else
+		local curWeight = this:GetSharedObjectProperty("Weight")
+		local totalWeight = math.max(1, stackCount*singleWeight)
+		local adjustWeightBy = totalWeight - curWeight
+
+		this:SetObjVar("StackCount", stackCount)
+		UpdateStackName(stackCount)
+
+		if(adjustWeightBy ~= 0) then
+			this:SetSharedObjectProperty("Weight", totalWeight)
+			--DebugMessage("Adjusting Stack Weight By  "..adjustWeightBy.." - Old: "..curWeight.." New: "..totalWeight)
+
+			local containedObj = this:ContainedBy()
+			if(containedObj ~= nil) then
+                --DebugMessage("(2) adjustWeightBy is "..tostring(adjustWeightBy))
+                if (not(containedObj:HasModule('container_magic'))) then
+                    containedObj:SendMessage("AdjustWeight", adjustWeightBy)
+                end
+			end
+		end
+	end
+end
