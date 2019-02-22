ee44bda3472975930408f426a01c591f38f2555f
Chris Chambers <cchambers@gmail.com>
2019-02-22 00:01:37 -0500

    this works

--- mods/NOS/scripts/colorwar_controller.lua
+++ mods/NOS/scripts/colorwar_controller.lua
@@ -1,9 +1,60 @@
--- RoundID
 
-mQueue = {}
 
 function HandleModuleLoaded() 
-    GlobalVarWrite("ColorWar.Controller", { primary = this })
+    CallFunctionDelayed(TimeSpan.FromSeconds(2), OpenRegistration)
+end
+
+function OpenRegistration()
+    GlobalVarDelete("ColorWar.Player", nil)
+    GlobalVarWrite("ColorWar.Registration", nil, function (record) 
+        record["open"] = true
+        return true
+    end)
+
+    ServerBroadcast("Color Wars registration open for the next ten (10) minutes! To queue, type: /cw", true)
+    
+    CallFunctionDelayed(TimeSpan.FromSeconds(5), function()
+        ServerBroadcast("Color Wars registration open for the next five (5) minutes! To queue, type: /cw", true)
+    end)
+
+    CallFunctionDelayed(TimeSpan.FromSeconds(10), function()
+        SummonPlayers()
+    end)
+end
+
+function SummonPlayers()
+    local players = GlobalVarRead("ColorWar.Queue")
+    local count = 0
+    if (players == nil) then
+        DebugMessage("No one is queued.")
+        CloseRegistration()
+        return
+    end
+    for player, t in pairs(players) do
+        if ( GlobalVarReadKey("User.Online", player) ) then
+            count = count + 1
+            player:SendMessageGlobal("GlobalSummon", this:GetObjVar("Destination"), this:GetObjVar("RegionAddress"))
+            DebugMessage("Summoning " .. player:GetName() .. " for Color Wars.")
+            GlobalVarWrite("ColorWar.Player", nil, function (record) 
+                record[this] = true
+                return true
+            end)
+        else
+            DebugMessage(player:GetName() .. " is no longer online.")
+        end
+    end
+    DebugMessage(tostring("Summoned " .. count .. " players for Color Wars."))
+
+    -- if less than 6 people, extend "OPEN" for 5 mins?
+    CloseRegistration()
+end
+
+function CloseRegistration()
+    GlobalVarDelete("ColorWar.Queue", nil)
+    GlobalVarWrite("ColorWar.Registration", nil, function (record) 
+        record["open"] = false
+        return true
+    end)
 end
 
 RegisterEventHandler(EventType.ModuleAttached, GetCurrentModule(), HandleModuleLoaded)
